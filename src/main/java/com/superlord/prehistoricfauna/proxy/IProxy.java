3183d33fdf79c633bb90e01f72bc67579c6c3658
superlord9362 <33046398+superlord9362@users.noreply.github.com>
2019-01-24 12:35:58 -0800

    Delete IProxy.java

--- src/main/java/com/superlord/prehistoricfauna/proxy/IProxy.java
+++
@@ -1,9 +0,0 @@
-package com.superlord.prehistoricfauna.proxy;
-
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
-public interface IProxy {
-    void preInit(FMLPreInitializationEvent event);
-    void registerItemRenderer(Item item, int meta, String id);
-}
