6b0878afdce2b37c59a176c9456fc003d14ea321
superlord9362 <33046398+superlord9362@users.noreply.github.com>
2023-07-18 16:19:24 -0700

    Fix files no longer in code
    These files were removed and don't do anything actually

---
+++ src/main/java/superlord/prehistoricfauna/common/blocks/FeederBlock.java
@@ -0,0 +1,11 @@
+package superlord.prehistoricfauna.common.blocks;
+
+import net.minecraft.world.level.block.Block;
+
+public class FeederBlock extends Block {
+
+	public FeederBlock(Properties p_49795_) {
+		super(p_49795_);
+	}
+
+}
