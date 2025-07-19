cb7b1b8f45d7f3368766bf49c84ec785b933e0c6
superlord9362 <33046398+superlord9362@users.noreply.github.com>
2025-07-19 07:40:18 -0700

    Update PFChunkGenerators.java

--- src/main/java/superlord/prehistoricfauna/init/PFChunkGenerators.java
+++ src/main/java/superlord/prehistoricfauna/init/PFChunkGenerators.java
@@ -2,7 +2,7 @@ package superlord.prehistoricfauna.init;
 
 import com.mojang.serialization.Codec;
 
-import net.minecraft.core.registries.Registries;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -13,7 +13,7 @@ import superlord.prehistoricfauna.common.world.chunkgen.PrehistoricChunkGenerato
 import superlord.prehistoricfauna.common.world.chunkgen.TriassicChunkGenerator;
 
 public class PFChunkGenerators {
-    public static final DeferredRegister<Codec<? extends ChunkGenerator>> REGISTER = DeferredRegister.create(Registries.CHUNK_GENERATOR, PrehistoricFauna.MOD_ID);
+    public static final DeferredRegister<Codec<? extends ChunkGenerator>> REGISTER = DeferredRegister.create(BuiltInRegistries.CHUNK_GENERATOR.key(), PrehistoricFauna.MOD_ID);
 
     public static final RegistryObject<Codec<TriassicChunkGenerator>> TRIASSIC_CHUNK_GENERATOR = REGISTER.register("triassic_chunk_generator", () -> TriassicChunkGenerator.CODEC);
     public static final RegistryObject<Codec<JurassicChunkGenerator>> JURASSIC_CHUNK_GENERATOR = REGISTER.register("jurassic_chunk_generator", () -> JurassicChunkGenerator.CODEC);
